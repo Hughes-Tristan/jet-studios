a71f4ace031dfa81933fab8a0b3d1884b123b8a7
Hughes-Tristan <Hughes-Tristan@users.noreply.github.com>
2024-11-21 18:15:29 -0800

    Character and NPC Update
    - Character Input fully working
    - NPC customer operations mostly working

--- Assets/Scripts/Customers.cs
+++ Assets/Scripts/Customers.cs
@@ -3,30 +3,74 @@
 // Last Updated: 11-16-24
 // Customers Class
 
+// the intended purpose of this class is to manage customer behavior.
+// it also provides methods for customer receiving orders and assigning customer seats.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Customers : MonoBehaviour
 {
-    [SerializeField] private Items itemOrdered;
+    private Items itemOrdered;
     private WaveManager waveManager;
+    private Transform seatAssigned;
+    private SpriteRenderer orderDisplay;
 
     // Start is called before the first frame update
     void Start()
     {
         // this calls for the the wavemanager instance so the player can earn money
         waveManager = WaveManager.Instance;
+        if (waveManager == null)
+        {
+            Debug.LogError("WaveManager.Instance is null");
+        }
+
+        // this gets the random item that the customer ordered
+        itemOrdered = ItemManager.Instance.getRandomItem();
+        if (itemOrdered == null)
+        {
+            Debug.LogError("itemOrdered is null");
+        }
+
+        // this finds and initializes the item display then assigns the sprite to display
+        Transform orderTransform = transform.Find("orderDisplay");
+        if (orderTransform != null)
+        {
+            Debug.Log("orderDisplay found!");
+            orderDisplay = orderTransform.GetComponent<SpriteRenderer>();
+            if (orderDisplay != null )
+            {
+                if(itemOrdered.itemIcon != null)
+                {
+                    orderDisplay.sprite = itemOrdered.itemIcon;
+                } else
+                {
+                    Debug.Log("error: itemicon is null");
+                }
+
+            }
+            else
+            {
+                Debug.Log("error: orderDisplay sprite render not found");
+            }
+        } else
+        {
+            Debug.Log("orderDisplay not found!");
+        }
     }
-    
-    // this  class checks to see if and order is received by the customer  or not
+
+    // this function checks to see if and order is received by the customer or not
     // if the items order is the same as the item given to the customer the player will earn money
-    //  otherwise the order received is not correct
+    // otherwise the order received is not correct
     public bool orderReceived(Items item)
     {
-        if(item = itemOrdered)
+        if(item == itemOrdered)
         {
             waveManager.EarnMoney(10);
+           
+            CustomerManager.instance.seatAvailable(seatAssigned);
             Destroy(gameObject);
             return true;
         }
@@ -34,5 +78,12 @@ public class Customers : MonoBehaviour
         {
             return false;
         }
+    }
+
+
+    // this is a form of a setter function that assigns the seat the the customer
+    public void assignSeat(Transform seat)
+    {
+        seatAssigned = seat;
     }
 }
