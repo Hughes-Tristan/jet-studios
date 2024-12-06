5555463fc4c003b0afcae64aa3d685cd61ec33a6
Hughes-Tristan <Hughes-Tristan@users.noreply.github.com>
2024-12-05 22:15:18 -0800

    Win Screen Added and Build Glitch Fixed
    - Added a win screen
    - fixed a glitch for when you build the project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance {  get; private set; }
+
+    public int bossKills = 0;
+    //public GameObject winScreen;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void bossKillCount()
+    {
+        bossKills++;
+        if(bossKills >= 10) {
+            setWinScreen();
+        }
+    }
+
+    void setWinScreen()
+    {
+        SceneManager.LoadScene("WinScreen");
+    }
+}
