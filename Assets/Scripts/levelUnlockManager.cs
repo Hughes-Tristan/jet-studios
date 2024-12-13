4bd8e27e666c4d47c9feb93d7ecb329e9720b59f
1102-Kanaan-Evita <evitakanaan@nevada.unr.edu>
2024-12-12 17:28:45 -0800

    level 2 unlock
    added a script to unlock level 2
    it should lock it again once players quit the game
    fixed home button on win screen but it doesn't look pretty yet

---
+++ Assets/Scripts/levelUnlockManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelUnlockManager : MonoBehaviour
+{
+    public Button level2Button; // Reference to the Level 2 button
+
+    void Start()
+    {
+        // Check if Level 2 is unlocked
+        if (PlayerPrefs.GetInt("Level2Unlocked", 0) == 1)
+        {
+            level2Button.interactable = true; // Enable the button
+            level2Button.onClick.AddListener(LoadLevel2); // Add the click event listener
+        }
+        else
+        {
+            level2Button.interactable = false; // Keep the button disabled
+        }
+    }
+
+    // This method will load Level 2 when the button is clicked
+    void LoadLevel2()
+    {
+        Debug.Log("Level 2 Button Clicked!");
+        SceneManager.LoadScene("Final_Level_2"); 
+    }
+}
--- Assets/Scripts/quitGame.cs
+++ Assets/Scripts/quitGame.cs
@@ -7,6 +7,11 @@ public class QuitGame : MonoBehaviour
         // Logs a message in the editor for testing purposes
         Debug.Log("Quit button clicked!");
 
+        // Reset the Level2Unlocked status to 0 before quitting
+        PlayerPrefs.SetInt("Level2Unlocked", 0);
+        PlayerPrefs.Save();  // Save the PlayerPrefs data to ensure the reset is persistent
+
+
         // Exits the application
         Application.Quit();
     }
